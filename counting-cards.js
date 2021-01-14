c7ed1f16c8070a3af6cc44308d28ada2e88e4b76
Ricardo <ricardog94@hotmail.com>
2021-01-14 00:50:23 -0600

    cartas

---
+++ counting-cards.js
@@ -0,0 +1,34 @@
+var count = 0;
+function cc(card) {
+
+switch (card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+    count--;
+    break;
+}
+if(count>0){
+  console.log(count,"positivooo");
+ return count+" Bet";
+}
+else if (count<=0){
+  console.log(count.toString(),"negaticoo");
+  return count+" Hold";
+  
+}
+
+
+ 
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
