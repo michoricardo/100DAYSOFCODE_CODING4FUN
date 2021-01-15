b8816ff6e243add9d2a400b6941667ed71a463ae
Ricardo <ricardog94@hotmail.com>
2021-01-15 01:35:54 -0600

    otro maś

---
+++ accessing-object-properties-with-bracket-notation.js
@@ -0,0 +1,12 @@
+// Setup
+/*Si el nombre tiene un espacio  se usa bracket notation   tambien se puede usar aunque la variable no tenga espacios*/
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
---
+++ accessing-object-properties-with-variables.js
@@ -0,0 +1,12 @@
+//defines una variable que tenga una propiedad en el objeto
+// y luego accedes a ese objeto mediante la variable
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+
+var playerNumber = 16;       // Change this line
+var player = testObj[playerNumber];   // Change this line
