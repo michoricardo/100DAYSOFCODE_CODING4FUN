4b87b83f8707520a10c84771a78e8c1eec27e9b7
Ricardo <ricardog94@hotmail.com>
2021-01-18 00:59:11 -0600

    algunos mas

---
+++ delete-properties-from-a-javascript-object.js
@@ -0,0 +1,11 @@
+// Setup
+/*Delete the "tails" property from myDog. You may use either dot or bracket notation.*/
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails
+// Only change code below this line
