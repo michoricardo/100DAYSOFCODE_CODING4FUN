8f0fc7ef4f9920529cd0c87f4628d41e86dae86f
Ricardo <ricardog94@hotmail.com>
2021-01-20 00:22:28 -0600

    objetcsss

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,28 @@
+
+/*Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.*/ 
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Michito",
+    "title": "Micho music",
+    "release_year": 1994,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": false
+  }
+  // Add a record here
+
+];
