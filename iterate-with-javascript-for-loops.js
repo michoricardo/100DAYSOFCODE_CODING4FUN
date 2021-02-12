a638eb0ec5fa38f897b6d15dfe47aa2d13638065
Ricardo <ricardog94@hotmail.com>
2021-02-12 14:09:40 -0600

    dops mas

---
+++ iterate-with-javascript-for-loops.js
@@ -0,0 +1,8 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i=1; i<6; i++){
+    myArray.push(i);
+}
+/*Use a for loop to work to push the values 1 through 5 onto myArray. */
---
+++ iterate-with-javascript-while-loops.js
@@ -0,0 +1,11 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var i = 5;
+while(i>-1){
+    myArray.push(i);
+    console.log(i);
+    i--;
+}
+/* Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.*/
