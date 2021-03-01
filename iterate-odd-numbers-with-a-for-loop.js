fd81b5c983e7e08d3d058f96ca6303f1650a91ed
Ricardo <ricardog94@hotmail.com>
2021-03-01 00:41:31 -0600

    nested fors and do while

---
+++ iterate-odd-numbers-with-a-for-loop.js
@@ -0,0 +1,8 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i=1; i<10; i+=2){
+myArray.push(i);
+}
+/* Push the odd numbers from 1 through 9 to myArray using a for loop.*/
